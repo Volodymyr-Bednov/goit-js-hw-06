3b3270df39c2136d8005ac839114018293977228
Volodymyr-Bednov <109081369+Volodymyr-Bednov@users.noreply.github.com>
2022-11-13 21:09:41 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-console.log(getRandomHexColor());
 
 const button = document.querySelector(".change-color");
 const textColor = document.querySelector("span.color");
 const bodyColor = document.querySelector("body");
-console.log();
 button.addEventListener("click", (ev) => {
-  textColor.textContent = getRandomHexColor();
-  bodyColor.style.backgroundColor = getRandomHexColor();
+  const hexColor = getRandomHexColor();
+  textColor.textContent = hexColor;
+  bodyColor.style.backgroundColor = hexColor;
 });
